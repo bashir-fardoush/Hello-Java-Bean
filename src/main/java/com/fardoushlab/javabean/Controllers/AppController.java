b1e17e7dcc189714d798cfb962729a13398704ad
bashir-fardoush <bashir.pure@gmail.com>
2020-01-18 15:35:20 +0600

    bean practice complete final commit

--- src/main/java/com/fardoushlab/javabean/Controllers/AppController.java
+++ src/main/java/com/fardoushlab/javabean/Controllers/AppController.java
@@ -7,24 +7,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fardoushlab.javabean.model.User;
+import com.fardoushlab.javabean.service.UserService;
 
 @Controller
 public class AppController {
 	
+//	@Autowired
+//	private User user;
+	
 	@Autowired
-	private User user;
-
-	@GetMapping("/index")
-	public String helloBean(Model model) {
-		model.addAttribute("user",user);		
+	UserService userService;
+//
+//	@GetMapping("/index")
+//	public String helloBean(Model model) {
+//	//	model.addAttribute("user",user);
+//		userService.showAllUser();
+//		return "index";
+//	}
+//	
+//	
+	@GetMapping("/show-all")
+	public String showAllUser(Model model) {
+		model.addAttribute("message","Showing all user");
+		userService.showAllUser();		
 		return "index";
+		
 	}
 	
-	
-	@GetMapping("/index2")
-	public String hello(@RequestParam("username") String username, Model model) {
-		user.setName(username);
-		model.addAttribute("user",user);
+	@GetMapping("/add-user")
+	public String addUser(@RequestParam("username") String username, Model model) {
+		
+		userService.createUser(username);
+		model.addAttribute("message","User created successfully");
+		return "index";		
+	}
+	@GetMapping("/remove-user")
+	public String removeUser(@RequestParam("username") String username, Model model) {
+		
+		userService.removeUserByName(username);
+		model.addAttribute("message","User removed successfully");
 		return "index";		
 	}
 	
