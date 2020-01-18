bf49d741269a97477b08802e7321266fd541ed3f
bashir-fardoush <bashir.pure@gmail.com>
2020-01-18 11:27:30 +0600

    project setup complete

---
+++ src/main/java/com/fardoushlab/javabean/AppRootConfig.java
@@ -0,0 +1,9 @@
+package com.fardoushlab.javabean;
+
+//import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppRootConfig {
+
+}
